c249b611afe8491d67fde19f8c25f75e6ca8f62c
3amasha <amr.amasha80@gmail.com>
2025-08-12 15:14:47 +0300

    Changing admin class

--- src/main/java/pages/admin/Admin.java
+++ src/main/java/pages/admin/Admin.java
@@ -1,6 +1,24 @@
 package pages.admin;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class Admin {
 
     //test
+    //changing admin class to test git
+    private final By locator = new By() {
+        @Override
+        public List<WebElement> findElements(SearchContext context) {
+            return List.of();
+        }
+
+        @Override
+        public String toString() {
+            return "Admin Locator";
+        }
+    };
 }
